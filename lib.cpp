979a34b8b6d3819ee2f78eacac196948ad535217
Termi11 <91840662+Termi11@users.noreply.github.com>
2021-12-11 16:43:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,22 @@
-#include "lib.h"
 #include <iostream>
+#include "lib.h"
 using namespace std;
-float max(const float A[], int dimensione) {
-    float massimo = A[0];
 
-    for (int i = 1; i < dimensione; i++) {
-       if(A[i]>massimo) {
-           massimo=A[i];
-       }
+
+float funzione( ) {
+    int i,n;
+    float   max;
+    cin>>n;
+    float array[n];
+    for (i = 0; i < n; i++) {
+        cin >> array[i];
     }
-    return massimo;
+    max = array[0];
+
+    for (i = 0; i < n; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
 }
